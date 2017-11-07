2ae655e4e9167dafb26e7c7fffd9b34f686d7347
lucas schmitz <lucas.schmitz@institutoivoti.com.br>
2017-11-07 19:44:03 -0200

    criação da tabela

--- Program.cs
+++ Program.cs
@@ -1,33 +1,13 @@
 using System;
 
-namespace console_dotnet
+namespace Brasileirao
 {
     class Program
     {
         static void Main(string[] args)
         {
-            String nome;
-            nome = "Grovy";
-            // Console.WriteLine("Opaaaaaaa");
-            Console.WriteLine(nome);
-
-            Double valor = 2.2;
-            Double quantidade = 6.888;
-            Double resultado = valor * quantidade;
-            Console.WriteLine(resultado);
-
-            Boolean maiorQueDez = (resultado > 10);
-            if (maiorQueDez) {
-                System.Console.WriteLine($"{resultado} é maior que dez");
-            } else {
-                System.Console.WriteLine($"{resultado} é menor que dez");
-            }
-                Grovy a = new Grovy();
-                System.Console.WriteLine(a.calc(6, 2, 3));
-                System.Console.WriteLine(a.calc2(10, 35, 22));
-                Acesso q = new Acesso();
-                q.numerogerado(0,9);
-            
+             Tabela tabela = new Tabela();
+            tabela.tabela();
         }
     }
 }
---
+++ Tabela.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Brasileirao
+{
+    class Tabela
+    {
+        public ListaTimes times = new ListaTimes();
+        public void tabela() {
+            System.Console.WriteLine("Tabela");
+            System.Console.WriteLine("1 - Adiciona times e pontuação/n 2 - Mostra o resultado da tabela");
+            
+        }
+    }
+}
