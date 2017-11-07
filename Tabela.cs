634203b5f9ad2a03081fc3c5642f5bd7736194fb
lucas schmitz <lucas.schmitz@institutoivoti.com.br>
2017-11-07 20:38:55 -0200

    continuação  da tabela

--- Tabela.cs
+++ Tabela.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Brasileirao
 {
@@ -7,8 +8,21 @@ namespace Brasileirao
         public ListaTimes times = new ListaTimes();
         public void tabela() {
             System.Console.WriteLine("Tabela");
-            System.Console.WriteLine("1 - Adiciona times e pontuação/n 2 - Mostra o resultado da tabela");
-            
+            System.Console.WriteLine("1 - Adiciona times e pontuação/n2 - Mostra o resultado da tabela");
+
+            while (Console.ReadKey().Key == ConsoleKey.Enter) {
+                System.Console.WriteLine("Informe o nome do time: ");
+                string time = Console.ReadLine();
+
+                 System.Console.WriteLine("Informe a pontuação do time: ");
+                int pont = Convert.ToInt32(Console.ReadLine());
+
+                this.times.Add(new Times(time, pont));
+            }
+
+            for (int i = 0; i < times.Count; i++) {
+                times[i].dados();
+            }
         }
     }
 }
