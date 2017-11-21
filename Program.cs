e568e925f1066c82d1dbed904c3297568d46dfd2
lucas schmitz <lucas.schmitz@institutoivoti.com.br>
2017-11-21 19:45:10 -0200

    Aperfeicoamento

--- Program.cs
+++ Program.cs
@@ -6,8 +6,25 @@ namespace Brasileirao
     {
         static void Main(string[] args)
         {
-             Tabela tabela = new Tabela();
-            tabela.tabela();
+            ConsoleKeyInfo a;
+             Tabela a = new Tabela();
+
+            do {
+                System.Console.WriteLine("Menu");
+                System.Console.WriteLine("Enter - Time e pontuação \n2 - Mostra a tabela");
+                System.Console.WriteLIne("Informe sua opção");
+                a = Console.ReadKey();
+                if (a.Key == ConsoleKey.D1) {
+                    System.Console.WriteLine("nome do time");
+                    String time = Console.ReadLine();
+                    System.Console.WriteLine("pontuação");
+                        String pont = Console.ReadLine();
+                        a.timea.Add(new Times(time, Int32.Parse(pont)));
+                        times.Sport(pont, a.pont);
+                        {
+                        } while (a.Key != Console)
+
+
         }
     }
 }
